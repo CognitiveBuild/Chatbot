45071379e6013b62a8454145d407f5d83550470e
Michael <mihui.net@outlook.com>
2016-11-10 23:21:47 +0800

    Removed unused code

--- src/com/ibm/cto/Configuration.java
+++ src/com/ibm/cto/Configuration.java
@@ -1,6 +1,5 @@
 package com.ibm.cto;
 
-import java.util.Iterator;
 import java.util.Set;
 
 import com.alibaba.fastjson.JSONArray;
@@ -13,7 +12,6 @@ public class Configuration {
 	public static final String SERVICE_SPEECH_TO_TEXT = "speech_to_text";
 	public static final String SERVICE_TEXT_TO_SPEECH = "text_to_speech";
 	public static final String SERVICE_CONVERSATION = "conversation";
-
 	public static final String TOKEN_API_URL = "https://stream.watsonplatform.net/authorization/api/v1/token";
 
 	public String SPEECH_TO_TEXT_API_URL = "https://stream.watsonplatform.net/speech-to-text/api";
@@ -102,52 +100,11 @@ public class Configuration {
 					System.out.println("### Empty service list ###");
 				}
 			}
-
-//			Iterator<String> iterator = vcapConfig.keySet().iterator();
-//			while(iterator.hasNext()){
-//				String keyString = iterator.next();
-//				JSONObject valueObject = vcapConfig.getJSONObject(keyString);
-//				System.out.println("### Key ###");
-//				System.out.println(keyString);
-//				System.out.println("### /Key ###");
-//
-//				if (keyString.startsWith(SERVICE_CONVERSATION)) {
-//					JSONObject credentials = queryObjectByKey(valueObject, "credentials");
-//					instance.CONVERSATION_USERNAME = credentials.get("username").toString();
-//					instance.CONVERSATION_PASSWORD = credentials.get("password").toString();
-//					instance.CONVERSATION_API_URL = credentials.get("url").toString();
-//				}
-//				else if(keyString.startsWith(SERVICE_SPEECH_TO_TEXT)) {
-//					JSONObject credentials = queryObjectByKey(valueObject, "credentials");
-//					instance.SPEECH_TO_TEXT_USERNAME = credentials.get("username").toString();
-//					instance.SPEECH_TO_TEXT_PASSWORD = credentials.get("password").toString();
-//					instance.SPEECH_TO_TEXT_API_URL = credentials.get("url").toString();
-//				}
-//				else if(keyString.startsWith(SERVICE_TEXT_TO_SPEECH)) {
-//					JSONObject credentials = queryObjectByKey(valueObject, "credentials");
-//					instance.TEXT_TO_SPEECH_USERNAME = credentials.get("username").toString();
-//					instance.TEXT_TO_SPEECH_PASSWORD = credentials.get("password").toString();
-//					instance.TEXT_TO_SPEECH_API_URL = credentials.get("url").toString();
-//				}
-//			}
 		}
 
 		return instance;
 	}
 
-	/**
-	 * Query JSONObject by indicating a key
-	 * 
-	 * @param config
-	 * @param key
-	 * @return JSONObject
-	 */
-	private static JSONObject queryObjectByKey(JSONObject config, String key) {
-		JSONArray services = config.getJSONArray(key);
-		JSONObject service = services.getJSONObject(0);
-		JSONObject queriedObject = service.getJSONObject(key);
-		return queriedObject;
-	}
 
 	/**
 	 * Get VCAP configurations
