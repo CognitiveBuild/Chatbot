2295c6ba6a2dcd92a3a78c0b0a214d2acaab4ea4
Michael <mihui.net@outlook.com>
2017-08-28 14:51:55 +0800

    Browser test

--- WebContent/js/talk-to-watson.js
+++ WebContent/js/talk-to-watson.js
@@ -450,10 +450,8 @@ var conversation_result, is_wating = false, controls = {
 		initToken('stt').then(function(t) {
 			sttToken = t;
 			converse('Hi Watson');
-			// var userAgent = window.navigator.userAgent.toLowerCase();
-			// if(userAgent.indexOf('chrome') != -1 || userAgent.indexOf('firefox') != -1){
-			// 	$mic.show();
-			// }
+			var userAgent = window.navigator.userAgent.toLowerCase();
+			$mic.show();
 		});
 		
 		scrollToInput();
